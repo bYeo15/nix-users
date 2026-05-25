065de216616a12d76032cfee4bf063bd0a935af5
Ben Y. <bjy777101@gmail.com>
2026-05-26 08:46:07 +1000

    [FEAT] : Updated ssh config to new format, added ssh-agent as a service

--- ben/packages/ssh.nix
+++ ben/packages/ssh.nix
@@ -3,6 +3,22 @@
 {
     programs.ssh = {
         enable = true;
-        matchBlocks = extlib.filterTagged "block" [ "all" "git" "homelab" ] config.sshConn;
+        settings = (extlib.filterTagged "block" [ "all" "git" "homelab" ] config.sshConn) // {
+            "*" = {
+                ForwardAgent = false;
+                AddKeysToAgent = false;
+                Compression = false;
+                ServerAliveInterval = 0;
+                ServerAliveCountMax = 3;
+                HashKnownHosts = false;
+                UserKnownHostsFile = "~/.ssh/known_hosts";
+                ControlPath = "~/.ssh/master-%r@%n:%p";
+                ControlPersist = "no";
+            };
+        };
+    };
+
+    services.ssh-agent = {
+        enable = true;
     };
 }
